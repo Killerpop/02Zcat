7b5cab79ed90d4f43f5aee4dab4650574baba584
Killerpop  Haxk.Ur <34385441+Killerpop@users.noreply.github.com>
2019-07-23 18:02:36 -0300

    Update 02Zcat.sh

--- 02Zcat.sh
+++ 02Zcat.sh
@@ -5,7 +5,7 @@ cat << !
 ───────────────────────▄▄──█░░░░░░░░░░░█──▄▄
 ──────────────────────█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█ 02Zcat
                                      0.2Zcat backdoor generator
-                                     By: @Killerpop http://www.haxkur.ml/
+                                     By: @Killerpop https://www.haxkur.ml/
 !
 
 echo -e '     \e[0;31mWARNING\e[0m'
